d0aed590ebe6a5fc58d46c9312aee6c3e7e0c6ae
tomjw64 <tom.jw64@gmail.com>
2020-01-05 16:47:39 -0600

    Edit navbar to include desired pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,8 +41,8 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <Link to="/" className="navbar-item" title="TXISB">
+              <img src={logo} alt="TXISB logo" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -60,26 +60,20 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/event">
+                Event
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/schedule">
+                Schedule
               </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+              <Link className="navbar-item" to="/register">
+                Register
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/tomjw64/txisb-netlify-cms"
                 target="_blank"
                 rel="noopener noreferrer"
               >
